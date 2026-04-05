e6065c13be084d8ce00f98522c79fb302b27613a
Tom Gregor <tgregor@ivsoftware.com>
2026-04-05 12:32:02 -0600

    ORIGIN NBHTLI TypeCacheExtensions

---
+++ IVSoftware.Portable.Common.MSTest/TestClass_TypeCache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVSoftware.Portable.Common.MSTest
+{
+    [TestClass]
+    public class TestClass_TypeCache
+    {
+
+        [TestMethod]
+        public void Test_TypeCache()
+        {
+        }
+    }
+}
