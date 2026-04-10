3e3cc7ceaf7c06e9e4ddece06b48fd811c2f8198
Tom Gregor <tgregor@ivsoftware.com>
2026-04-10 16:22:38 -0600

    ADDED UNIT TEST COVERAGE for throw polarity.

--- IVSoftware.Portable.Common.MSTest/TestClass_Throw.cs
+++ IVSoftware.Portable.Common.MSTest/TestClass_Throw.cs
@@ -16,7 +16,7 @@ namespace IVSoftware.Portable.Common.MSTest
             var builderThrow = new List<string>();
             void localOnBeginThrowOrAdvise(object? sender, Throw e)
             {
-                builderThrow.Add(e.Message);
+                builderThrow.Add($"Handled={e.Handled} {e.Message}");
                 e.Handled = true;
             }
             #endregion L o c a l F x
@@ -30,19 +30,98 @@ namespace IVSoftware.Portable.Common.MSTest
                     Throw.BeginThrowOrAdvise -= localOnBeginThrowOrAdvise;
                 });
 
-            this.ThrowHard<InvalidOperationException>();
+            // This format checks the success of handling it within the local handler.
+            Assert.IsTrue(this.ThrowHard<InvalidOperationException>().Handled);
 
-
-            actual = string.Join(Environment.NewLine, builderThrow);
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
             actual.ToClipboardExpected();
             { }
             expected = @" 
-InvalidOperationException";
+Handled=False InvalidOperationException"
+            ;
 
             Assert.AreEqual(
                 expected.NormalizeResult(),
                 actual.NormalizeResult(),
-                "Expecting logged throw messages."
+                "Expecting NOT HANDLED."
+            );
+
+            // This format checks the success of handling it within the local handler.
+            Assert.IsTrue(
+                this.ThrowFramework<NotSupportedException>()
+                .Handled);
+
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+Handled=False NotSupportedException"
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting NOT HANDLED."
+            );
+
+            this.ThrowFramework<NotSupportedException>(@throw: false);
+
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+Handled=True NotSupportedException"
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting HANDLED."
+            );
+
+            this.ThrowSoft<NotSupportedException>(@throw: false);
+
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+Handled=True NotSupportedException"
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting HANDLED."
+            );
+
+            this.RethrowHard(new NullReferenceException());
+
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+Handled=False Object reference not set to an instance of an object."
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting NOT HANDLED."
+            );
+
+            this.RethrowSoft(new NullReferenceException());
+
+            actual = string.Join(Environment.NewLine, builderThrow); builderThrow.Clear();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+Handled=True Object reference not set to an instance of an object."
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting HANDLED."
             );
         }
     }
